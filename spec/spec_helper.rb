2401720a30e6b506becd86af2f6704f4598439f1
Andy Maleh <andy.am@gmail.com>
2013-11-26 15:40:29 -0500

    Disabled coveralls in Ruby 1.8.7 as it is not supported in versions below 1.9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,10 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-require 'coveralls'
-Coveralls.wear!
+if RUBY_VERSION >= "1.9"
+  require 'coveralls'
+  Coveralls.wear!
+end
 require File.join(File.dirname(__FILE__), '..', 'lib', 'abstract_feature_branch')
 AbstractFeatureBranch.logger.level = Logger::WARN
 AbstractFeatureBranch.load_application_features
