4f8bf856ccd181b00baca7c1a319344a4f5dd60b
Andy Maleh <andy.am@gmail.com>
2013-11-26 16:37:20 -0500

    More fixes to make coveralls work with all versions of Ruby on Travis CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,9 +8,13 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-if RUBY_VERSION >= "1.9"
-  require 'coveralls'
-  Coveralls.wear!
+if RUBY_VERSION >= '1.9'
+  begin
+    require 'coveralls'
+    Coveralls.wear!
+  rescue
+    #no op to support Ruby 1.8.7, ree and Rubinius which do not support Coveralls
+  end
 end
 require File.join(File.dirname(__FILE__), '..', 'lib', 'abstract_feature_branch')
 AbstractFeatureBranch.logger.level = Logger::WARN
