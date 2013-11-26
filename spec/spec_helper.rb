621242457237625e87791453427c0330f10d22fc
Andy Maleh <andy.am@gmail.com>
2013-11-26 09:17:12 -0500

    Added Coveralls test coverage integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
+Coveralls.wear!
 require File.join(File.dirname(__FILE__), '..', 'lib', 'abstract_feature_branch')
 AbstractFeatureBranch.logger.level = Logger::WARN
 AbstractFeatureBranch.load_application_features
