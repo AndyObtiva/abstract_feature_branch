6fa6a807f9306bc2e24cb12041e7c79dd7390a61
Andy Maleh <andy.am@gmail.com>
2014-01-13 11:30:14 -0500

    Added basic support for per-user feature configuration. Removed support for true/false branching

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'deep_merge', '1.0.0', :require => false #avoid loading into memory to use deep_merge only if Rails is unavailable
+gem 'redis', '3.0.6', :require => false
 
 group :development do
   gem 'jeweler', '1.8.8'
--- lib/ext/feature_branch.rb
+++ lib/ext/feature_branch.rb
@@ -1,24 +1,25 @@
 class Object
   raise 'Abstract feature branch conflicts with another Ruby library' if respond_to?(:feature_branch)
-  def self.feature_branch(feature_name, branches = {}, &feature_work)
-    branches[:true] ||= feature_work
-    branches[:false] ||= lambda {}
-    feature_branch_symbol_value = (!!feature_enabled?(feature_name)).to_s.to_sym
-    branches[feature_branch_symbol_value].call
+  def self.feature_branch(feature_name, user_id = nil, &feature_work)
+    if feature_enabled?(feature_name, user_id)
+      feature_work.call
+    end
   end
 
   raise 'Abstract feature branch conflicts with another Ruby library' if respond_to?(:feature_enabled?)
-  def self.feature_enabled?(feature_name)
-    AbstractFeatureBranch.application_features[feature_name.to_s.downcase]
+  def self.feature_enabled?(feature_name, user_id = nil)
+    normalized_feature_name = feature_name.to_s.downcase
+    AbstractFeatureBranch.application_features[normalized_feature_name] &&
+      (user_id.nil? || AbstractFeatureBranch.user_features_storage.sismember("#{AbstractFeatureBranch::ENV_FEATURE_PREFIX}#{normalized_feature_name}", user_id))
   end
 
   raise 'Abstract feature branch conflicts with another Ruby library' if Object.new.respond_to?(:feature_branch)
-  def feature_branch(feature_name, branches = {}, &feature_work)
-    Object.feature_branch(feature_name.to_s, branches, &feature_work)
+  def feature_branch(feature_name, user_id = nil, &feature_work)
+    Object.feature_branch(feature_name.to_s, user_id, &feature_work)
   end
 
   raise 'Abstract feature branch conflicts with another Ruby library' if Object.new.respond_to?(:feature_enabled?)
-  def feature_enabled?(feature_name)
-    Object.feature_enabled?(feature_name.to_s)
+  def feature_enabled?(feature_name, user_id = nil)
+    Object.feature_enabled?(feature_name.to_s, user_id)
   end
 end
